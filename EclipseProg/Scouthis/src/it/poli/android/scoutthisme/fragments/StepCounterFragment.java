86b8c54ffb87f4cf3bc96e54eac66c41387cf24e
edoardo90 <edoardo.pasi@mail.polimi.it>
2014-06-12 11:03:54 +0200

    gif prova

--- EclipseProg/Scouthis/src/it/poli/android/scoutthisme/fragments/StepCounterFragment.java
+++ EclipseProg/Scouthis/src/it/poli/android/scoutthisme/fragments/StepCounterFragment.java
@@ -2,11 +2,16 @@ package it.poli.android.scoutthisme.fragments;
 
 import it.poli.android.scouthisme.R;
 import it.poli.android.scoutthisme.tools.CircleButton;
+import it.poli.android.scoutthisme.tools.GifAnimationDrawable;
+
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class StepCounterFragment extends StepCounterFragmentArchetype
 {
@@ -20,10 +25,33 @@ public class StepCounterFragment extends StepCounterFragmentArchetype
 		return rootView;
 	}
 	
+	private void setGifImage()
+	{
+		
+		
+		ImageView imgGigiImageView;
+		GifAnimationDrawable gigDagAnimation;
+		
+		imgGigiImageView = (ImageView)this.getActivity().findViewById(R.id.step_counter_gigidag);
+
+		try{
+			
+			gigDagAnimation = new GifAnimationDrawable(getResources().openRawResource(R.raw.img_gigi_dag));
+			gigDagAnimation.setOneShot(false);
+			imgGigiImageView.setImageDrawable(gigDagAnimation);			
+		}catch(IOException ioe){
+			
+		}
+	}
+	
+	
 	@Override
 	public void onResume()
 	{
 		super.onResume();
+		
+		
+		this.setGifImage();
 		
 		CircleButton btnNewRun = (CircleButton)rootView.findViewById(R.id.step_button_newrun);
 		btnNewRun.setOnClickListener(new OnClickListener()
